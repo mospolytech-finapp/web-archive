884354a876923c80aa94a756387d20842565aaf9
f1xgun <msnzlmm@mail.ru>
2023-05-01 13:33:18 +0300

    fix: попытка исправления использования переменных окружения

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -30,6 +30,7 @@ const AuthForm = () => {
   const [loginError, setLoginError] = useState('')
 
   const onSubmit = (data: FieldValues) => {
+    console.log(import.meta.env)
     UserDataService.login({
       username: data.username,
       password: data.password
--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -1,15 +1,7 @@
 import axios from 'axios'
 
-let baseURL = ''
-
-if (import.meta.env.MODE === 'development') {
-  baseURL = import.meta.env.VITE_API_URL || ''
-} else if (import.meta.env.MODE === 'production') {
-  baseURL = process.env.API_URL || ''
-}
-
 export default axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
