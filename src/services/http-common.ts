7e9705009289d7ad782714f0231f22a9af59f353
f1xgun <msnzlmm@mail.ru>
2023-05-01 13:20:27 +0300

    fix: попытка исправления использования переменных окружения

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
+let baseURL = ''
+
+if (import.meta.env.MODE === 'development') {
+  baseURL = import.meta.env.VITE_API_URL || ''
+} else if (import.meta.env.MODE === 'production') {
+  baseURL = process.env.API_URL || ''
+}
+
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL,
   headers: {
     'Content-Type': 'application/json'
   }
