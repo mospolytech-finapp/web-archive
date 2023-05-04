234d43aea574450b5644a9ba8ccc6f516b0c4fb1
f1xgun <msnzlmm@mail.ru>
2023-05-04 10:55:26 +0300

    feat: добавил методы для работы с транзакциями целей с сервером

--- src/services/goal-service.ts
+++ src/services/goal-service.ts
@@ -1,4 +1,5 @@
 import IGoalData from 'types/goal'
+import IGoalTransactionData from 'types/goal_transaction'
 
 import http from '../services/http-common'
 
@@ -7,7 +8,7 @@ class GoalDataService {
     return await http.get<Array<IGoalData>>('/finance/goals')
   }
 
-  async get(id: string) {
+  async get(id: number) {
     return await http.get<IGoalData>(`/finance/goals/${id}`)
   }
 
@@ -15,12 +16,24 @@ class GoalDataService {
     return await http.post<IGoalData>('/finance/goals/', data)
   }
 
-  async update(data: IGoalData, id: string) {
+  async update(data: IGoalData, id: number) {
     return await http.put<IGoalData>(`/finance/goals/${id}`, data)
   }
 
-  async delete(id: string) {
+  async delete(id: number) {
     return await http.delete(`/finance/goals/${id}`)
+  }
+
+  async getAllTransactions(goalId: number) {
+    return await http.get<Array<IGoalTransactionData>>(`/finance/goals/${goalId}/transactions/`)
+  }
+
+  async createTransaction(goalId: number, data: IGoalTransactionData) {
+    return await http.post<IGoalTransactionData>(`/finance/goals/${goalId}/transactions`, data)
+  }
+
+  async getTransaction(goalId: number, id: number) {
+    return await http.get<IGoalTransactionData>(`/finance/goals/${goalId}/transactions/${id}`)
   }
 }
 
