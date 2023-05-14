6adce1deaa1439b9adde23afccb78ae3afac015b
nik300904 <nikitak3009@gmail.com>
2023-05-14 23:01:19 +0300

    fix(Goal): фикс ширины донат гоал

--- src/components/ui/GoalProgressBar.tsx
+++ src/components/ui/GoalProgressBar.tsx
@@ -15,7 +15,7 @@ const GoalProgressBar = ({ progress, progressText }: GoalProgressBarProps) => {
   // rounded-full bg-gradient-to-r md:rounded-r-3xl
 
   return (
-    <div className="from-progressbar-nocompleted-green to-progressbar-nocompleted-black relative h-11 w-full rounded-full bg-gradient-to-r xl:rounded-l-3xl">
+    <div className="from-progressbar-nocompleted-green to-progressbar-nocompleted-black relative h-11 w-full xl:w-96 rounded-full bg-gradient-to-r xl:rounded-l-3xl">
       <div
         className="from-progressbar-completed-green-from to-progressbar-completed-green-to flex h-full items-center justify-center rounded-r-3xl bg-gradient-to-r transition-all duration-300"
         style={{ width: `${width}%` }}
