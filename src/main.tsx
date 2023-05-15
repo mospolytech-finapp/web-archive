690db4c193ef558bdaf3a63453046b827deca0b8
dangannn <dn14072004@mail.ru>
2023-05-15 12:48:48 +0300

    fix: авторизация с регистрацией по центру

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AuthForm from './components/AuthForm'
 import RegisterForm from './components/RegisterForm'
 import App from './components/App'
+import Home from './components/Home'
 
 createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        {/*<Route element={<AuthForm />} path='*'>*/}
+        {/*  <Route element={<Home />} path='/home' />*/}
+        {/*  <Route element={<RegisterForm />} path='register' />*/}
+        {/*</Route>*/}
         <Route element={<App />} path="*">
           <Route element={<AuthForm />} path="auth" />
           <Route element={<RegisterForm />} path="register" />
