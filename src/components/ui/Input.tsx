798692e28ae97aa9a9d1213f0b905d74edccfbc0
Ivan <vakin@proton.me>
2023-05-16 22:13:35 +0300

    фикс размера текста в поле ввода даты

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -125,7 +125,7 @@ const Input = ({ ...props }: InputProps) => {
           <input
             {...props.register(props.name)}
             aria-invalid={props.error}
-            className={`text-true-gray-900 placeholder:text-light-gray max-h-12 w-full rounded-full py-4 px-6 text-xs font-normal ${
+            className={`text-true-gray-900 placeholder:text-light-gray max-h-12 w-full rounded-full py-4 px-6 text-base font-normal ${
               props.error ? 'bg-error border-light-red border-2' : 'bg-[#ECECEC]}'
             }
               ${props.disabled ? 'bg-white text-black/50' : ''}
